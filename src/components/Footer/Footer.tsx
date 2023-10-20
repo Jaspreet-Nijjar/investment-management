4d3984334675335d8df989cd983c29fb8d022d39
Jaspreet <jaspreetkn92@gmail.com>
2023-10-20 07:52:49 +0100

    created the footer component

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="bg-black text-white text-center p-3">
+      <p>Created By Jaspreet Nijjar</p>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from 'react-router-dom';
+import Footer from '../components/Footer/Footer';
 
 const RootLayout = () => {
   return (
     <div>
       Root
       <Outlet />
+      <Footer />
     </div>
   );
 };
