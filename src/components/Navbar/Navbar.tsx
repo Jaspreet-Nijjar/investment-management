d37bd885ac0af5498411441fafcebf50b559c107
Jaspreet <jaspreetkn92@gmail.com>
2023-10-22 14:09:29 +0100

    added the icons and links to the navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -11,5 +11,48 @@ import Logo from '../images/logo.png';
 import React from 'react';
 
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <nav className="bg-black h-screen fixed left-0 w-58 p-8">
+      <div className="logo mb-10">
+        <Link to="/">
+          <img src={Logo} width="150" alt="virtual portfolio main logo" />
+        </Link>
+      </div>
+
+      <div className="flex content-center text-white hover:text-orange-500 transition ease-out duration-500 p-2">
+        <AiOutlineHome size={20} />
+        <Link to="/" className="mx-2">
+          Home
+        </Link>
+      </div>
+
+      <div className="flex content-center text-white hover:text-orange-500 transition ease-out duration-500 p-2">
+        <BsPieChart size={20} />
+        <Link to="/portfolio" className="mx-2">
+          Portfolio
+        </Link>
+      </div>
+
+      <div className="flex content-center text-white hover:text-orange-500 transition ease-out duration-500 p-2">
+        <BiMoneyWithdraw size={20} />
+        <Link to="/stocks" className="mx-2">
+          Stocks
+        </Link>
+      </div>
+
+      <div className="flex content-center text-white hover:text-orange-500 transition ease-out duration-500 p-2">
+        <AiOutlineFundProjectionScreen size={20} />
+        <Link to="/cryptocurrencies" className="mx-2">
+          Cryptocurrencies
+        </Link>
+      </div>
+
+      <div className="flex content-center text-white hover:text-orange-500 transition ease-out duration-500 p-2">
+        <AiOutlineBulb size={20} />
+        <Link to="/news" className="mx-2">
+          News
+        </Link>
+      </div>
+    </nav>
+  );
 }
