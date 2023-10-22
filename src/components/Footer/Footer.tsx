c244a9e5dc83956f1c8c6935c9f18d44d4628778
Jaspreet <jaspreetkn92@gmail.com>
2023-10-22 14:14:27 +0100

    fixed footer styling to position to bottom

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="bg-black text-white text-center p-3">
+    <footer className="fixed bottom-0 w-screen bg-black text-white text-center p-3">
       <p>Created By Jaspreet Nijjar</p>
     </footer>
   );
