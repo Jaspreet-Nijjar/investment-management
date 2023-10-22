73137f667f29ba8827a33a85d0a5cdfe5dae5dd2
Jaspreet <jaspreetkn92@gmail.com>
2023-10-22 14:18:11 +0100

    added hover effect to footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="fixed bottom-0 w-screen bg-black text-white text-center p-3">
+    <footer className="fixed bottom-0 w-screen bg-black text-white text-center p-3 hover:text-orange-500 transition ease-in-out duration-300">
       <p>Created By Jaspreet Nijjar</p>
     </footer>
   );
