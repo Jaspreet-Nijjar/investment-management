e247f4b2767eea8e33f1027d8bfa4db7c869de4b
Jaspreet <jaspreetkn92@gmail.com>
2023-10-22 14:08:32 +0100

    imported logo and react icons

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,15 @@
+import {
+  AiOutlineHome,
+  AiOutlineBulb,
+  AiOutlineFundProjectionScreen,
+} from 'react-icons/ai';
+import { BiMoneyWithdraw } from 'react-icons/bi';
+import { BsPieChart } from 'react-icons/bs';
+import { Link } from 'react-router-dom';
+import Logo from '../images/logo.png';
+
+import React from 'react';
+
+export default function Navbar() {
+  return <div>Navbar</div>;
+}
