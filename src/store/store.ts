336924e006069211edbdae425716be0235027c5c
Jaspreet <jaspreetkn92@gmail.com>
2023-10-31 09:58:12 +0000

    added searchBar to store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { coinGeckoApi } from '../services/cryptoAPI';
+import searchBarReducer from '../components/SearchBar/searchBarSlice';
 
 const store = configureStore({
   reducer: {
     [coinGeckoApi.reducerPath]: coinGeckoApi.reducer,
+    searchBar: searchBarReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(coinGeckoApi.middleware),
