5f5492d7bd0d653529d62903bd67b5969f22b110
Jaspreet <jaspreetkn92@gmail.com>
2023-11-26 14:14:51 +0000

    created the sparkline chart to reflect 7 day prices cryptocurrency graphs in the cryptocurrency table

---
+++ src/components/SparkLineChart/SparkLineChart.tsx
@@ -0,0 +1,67 @@
+import { useGetCoinHistoryQuery } from '../../services/cryptoAPI';
+import { Line } from 'react-chartjs-2';
+import 'chart.js/auto';
+
+const SparkLineChart = ({ coinId }) => {
+  const { data: coinHistoryData } = useGetCoinHistoryQuery(coinId);
+
+  let prices = '';
+  let timestamps = '';
+
+  if (!coinHistoryData) {
+    return 'Loading...';
+  }
+
+  if (coinHistoryData) {
+    prices = coinHistoryData?.prices.map((priceData: number[]) => priceData[1]);
+    timestamps = coinHistoryData?.prices.map(
+      (priceData: number[]) => priceData[0]
+    );
+  }
+
+  const chartData = {
+    labels: timestamps,
+    datasets: [
+      {
+        label: 'Price',
+        data: prices,
+        borderColor:
+          prices[0] > prices[prices.length - 1] ? 'red' : 'lightgreen',
+        borderWidth: 1,
+        backgroundColor: 'rgba(255, 99, 132, 0.5)',
+        pointRadius: 0,
+        pointHoverRadius: 0,
+      },
+    ],
+  };
+
+  const options = {
+    plugins: {
+      legend: {
+        display: false,
+      },
+      tooltip: {
+        enabled: false,
+      },
+      title: {
+        display: false,
+      },
+    },
+    scales: {
+      x: {
+        display: false,
+      },
+      y: {
+        display: false,
+      },
+    },
+  };
+
+  return (
+    <div className="hidden lg:block flex justify-center">
+      <Line data={chartData} options={options} />
+    </div>
+  );
+};
+
+export default SparkLineChart;
