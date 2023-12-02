95f7a3090449a0d2111fcf4ed6725bd641dc8f2c
Jaspreet <jaspreetkn92@gmail.com>
2023-12-02 16:05:33 +0000

    added descriptive text for the TrendingCoins component

--- src/components/TrendingCoins/TrendingCoins.tsx
+++ src/components/TrendingCoins/TrendingCoins.tsx
@@ -12,6 +12,14 @@ export const TrendingCoins = () => {
     <section className="p-4 mt-4 mr-4 border border-gray-200 rounded md:w-96">
       <h2 className="text-center font-bold">Trending Coins</h2>
       <TrendingCoinRowHeader />
+      <p className="hidden md:block text-sm mt-2 text-center">
+        Discover the most trending cryptocurrencies today!{' '}
+        <span className="font-bold">
+          {data.coins[0].item.name}, {data.coins[1].item.name} and{' '}
+          {data.coins[2].item.name}
+        </span>{' '}
+        have been the highest ranking searches in the last 3 hours.
+      </p>
     </section>
   );
 };
