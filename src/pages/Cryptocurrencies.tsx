3976f5c3356dc58aab29bc152f99393de2d4c297
Jaspreet <jaspreetkn92@gmail.com>
2023-12-03 19:30:34 +0000

    added text to the Cryptocurrencies Page and added Poppins font

--- src/pages/Cryptocurrencies.tsx
+++ src/pages/Cryptocurrencies.tsx
@@ -3,7 +3,13 @@ import SearchBar from '../components/SearchBar/SearchBar';
 
 const Cryptocurrencies = () => {
   return (
-    <div className="ml-36">
+    <div className="ml-36 pt-6">
+      <h1 className="text-center text-2xl font-semibold">
+        All Cryptocurrencies
+      </h1>
+      <p className="text-center">
+        Discover and explore our full list of available cryptocurrencies
+      </p>
       <SearchBar placeholder="Search Cryptocurrencies" />
       <CryptocurrencyTable />
     </div>
