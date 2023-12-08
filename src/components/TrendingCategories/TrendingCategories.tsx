96a6b4277dcdbf77189d8e43581f10e9b032458b
Jaspreet <jaspreetkn92@gmail.com>
2023-12-08 14:26:03 +0000

    added text for the trending categories component

--- src/components/TrendingCategories/TrendingCategories.tsx
+++ src/components/TrendingCategories/TrendingCategories.tsx
@@ -1,7 +1,22 @@
+import { useGetTrendingCoinsQuery } from '../../services/cryptoAPI';
+
 const TrendingCategories = () => {
+  const { data, isLoading } = useGetTrendingCoinsQuery(1);
+
+  const trendingCategories = data && data.categories.slice(0, 5);
+  console.log(trendingCategories);
+
+  if (isLoading) {
+    return <div>Is Loading...</div>;
+  }
+
   return (
     <section className="mx-auto ml-4 mt-4 mr-4 border border-gray-200 rounded md:w-96">
       <h2 className="text-center font-bold">Trending Categories</h2>
+      <p className="hidden md:block text-sm p-2 text-center">
+        View the highest cryptocurrency categories currently trending based on
+        market capitalisation.
+      </p>
     </section>
   );
 };
--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,11 +1,15 @@
 // import { CoinSummaryData } from '../components/CoinSummaryData/CoinSummaryData';
+import TrendingCategories from '../components/TrendingCategories/TrendingCategories';
 import { TrendingCoins } from '../components/TrendingCoins/TrendingCoins';
 
 const Homepage = () => {
   return (
     <div className="ml-60">
+      <main className="flex">
+        <TrendingCoins />
+        <TrendingCategories />
+      </main>
       {/* <CoinSummaryData /> */}
-      <TrendingCoins />
     </div>
   );
 };
