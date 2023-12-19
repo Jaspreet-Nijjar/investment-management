e0028538f728c0940ae0d05a780b69646ade0f91
Jaspreet <jaspreetkn92@gmail.com>
2023-12-19 08:41:31 +0000

    created circulating supply / total supply progress bar

--- src/components/CryptocurrencyTable/CoinRow.tsx
+++ src/components/CryptocurrencyTable/CoinRow.tsx
@@ -1,6 +1,6 @@
 import SparkLineChart from '../../components/SparkLineChart/SparkLineChart';
 import millify from 'millify';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import { IoMdArrowDropup } from 'react-icons/io';
 import { IoMdArrowDropdown } from 'react-icons/io';
 
@@ -16,6 +16,8 @@ interface CoinProps {
   price_change_percentage_24h: number;
   price_change_percentage_1h_in_currency: number;
   price_change_percentage_7d_in_currency: number;
+  circulating_supply: number;
+  total_supply: number;
 }
 
 interface CoinRowProps {
@@ -95,13 +97,31 @@ export default function CoinRow({ coin }: CoinRowProps) {
           ${millify(coin.market_cap)}
         </div>
 
+        <div>
+          <div className="flex justify-around text-xs">
+            <p>${millify(coin.circulating_supply)}</p>
+            <p>${millify(coin.total_supply)}</p>
+          </div>
+          <div className="relative w-28 h-2 bg-gray-200 rounded mx-auto">
+            <div
+              className="absolute left-0 top-0 h-full bg-orange-300 rounded"
+              style={{
+                width: `${(
+                  (coin.circulating_supply / coin.total_supply) *
+                  100
+                ).toFixed(2)}%`,
+              }}
+            ></div>
+          </div>
+        </div>
+
         <SparkLineChart coinId={coin.id} />
-        <Link
+        {/* <Link
           className="hidden xl:block text-center text-orange-300 hover:text-orange-400 transition ease-in duration-300 font-bold"
           to={`/cryptocurrencies/crypto/${coin.id}`}
         >
           Details
-        </Link>
+        </Link> */}
       </section>
     </div>
   );
