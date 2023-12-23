57c9efb9d72095257d7d21dbdaca43d9baab5d5c
Jaspreet <jaspreetkn92@gmail.com>
2023-12-23 11:58:10 +0000

    created the MarketCapCard component

--- src/components/CryptocurrencyCards/MarketCapCard.tsx
+++ src/components/CryptocurrencyCards/MarketCapCard.tsx
@@ -1,5 +1,8 @@
 import { useGetCoinSummaryDataQuery } from '../../services/cryptoAPI';
 import LoadingSpinner from '../../shared/LoadingSpinner/LoadingSpinner';
+import { formatNumber } from '../../utilities/formatNumber';
+import { IoMdArrowDropup } from 'react-icons/io';
+import { IoMdArrowDropdown } from 'react-icons/io';
 
 const MarketCapCard = () => {
   const { data, isLoading, isError } = useGetCoinSummaryDataQuery(1);
@@ -13,8 +16,27 @@ const MarketCapCard = () => {
   }
 
   return (
-    <div className="border border-gray-200 rounded md:w-96">
-      <h1>{data.total_market_cap}</h1>
+    <div className="ml-64 mt-4 border p-4 border-gray-200 rounded md:w-max">
+      <h1 className="text-xl font-bold tracking-wide">
+        ${formatNumber(data.data.total_market_cap.usd)}
+      </h1>
+      <div className="flex justify-center items-center">
+        <p className="text-gray-500 text-md">Market Capitalisation</p>
+        {data.data.market_cap_change_percentage_24h_usd > 0 ? (
+          <IoMdArrowDropup size={20} className="text-green-500" />
+        ) : (
+          <IoMdArrowDropdown size={20} className="text-red-500" />
+        )}
+        <p
+          className={`text-sm font-bold ${
+            data.data.market_cap_change_percentage_24h_usd > 0
+              ? 'text-green-500'
+              : 'text-red-500'
+          }`}
+        >
+          {data.data.market_cap_change_percentage_24h_usd.toFixed(1)}%
+        </p>
+      </div>
     </div>
   );
 };
