77061565dd53d2876c14ccc20032c1412984ee92
Jaspreet <jaspreetkn92@gmail.com>
2024-01-01 16:18:37 +0000

    revised the shared Button component

--- src/shared/Button.tsx
+++ src/shared/Button.tsx
@@ -1,16 +1,27 @@
 import { Link } from 'react-router-dom';
+import { ButtonHTMLAttributes } from 'react';
 
-interface ButtonProps {
-  children: React.ReactNode;
-  to: string;
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  to?: string;
 }
 
-const Button: React.FC<ButtonProps> = ({ children, to }) => {
-  return (
-    <Link className="text-sm bg-black text-white px-2 py-1 rounded" to={to}>
-      {children}
-    </Link>
-  );
+const Button: React.FC<ButtonProps> = ({ children, to, ...buttonProps }) => {
+  if (to) {
+    return (
+      <Link className="text-sm bg-black text-white px-2 py-1 rounded" to={to}>
+        {children}
+      </Link>
+    );
+  } else {
+    return (
+      <button
+        className="text-sm bg-black text-white px-2 py-1 rounded"
+        {...buttonProps}
+      >
+        {children}
+      </button>
+    );
+  }
 };
 
 export default Button;
