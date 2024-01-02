5d708eb5d0d628c54ddbfebb03d6567e1ae996bd
Jaspreet <jaspreetkn92@gmail.com>
2024-01-02 08:01:58 +0000

    created BuyCryptoFormSchema for the form component

--- src/components/Forms/BuyCryptocurrencyForm.tsx
+++ src/components/Forms/BuyCryptocurrencyForm.tsx
@@ -1,4 +1,23 @@
+import { useForm, Controller } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { z } from 'zod';
+import { useGetCoinsQuery } from '../../services/cryptoAPI';
+import LoadingSpinner from '../../shared/LoadingSpinner';
+
 const BuyCryptocurrencyForm = () => {
+  const { data, isLoading } = useGetCoinsQuery(1);
+
+  if (isLoading) {
+    return <LoadingSpinner />;
+  }
+
+  const BuyCryptoFormSchema = z.object({
+    coin: z.string().min(1),
+    amount: z
+      .number()
+      .min(1, { message: 'You must purchase at least one unit' }),
+  });
+
   return <div>Form</div>;
 };
 
